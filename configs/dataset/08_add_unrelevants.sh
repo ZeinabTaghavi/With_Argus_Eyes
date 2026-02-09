91dd735598d82a55a30a32e1a4d0ce136c59e755
Zeinab Taghavi <zeinabtaghavi1377@gmail.com>
2026-02-09 10:41:37 +0100

    updated

--- configs/dataset/08_add_unrelevants.sh
+++ configs/dataset/08_add_unrelevants.sh
@@ -8,6 +8,6 @@ source "${THIS_DIR}/00_env.sh"
 PYTHON=${PYTHON:-python}
 
 "${PYTHON}" "${WORKSPACE_ROOT}/scripts/dataset/08_add_unrelevants.py" \
-  --all_relation_tags "${ARGUS_INTERIM_ROOT}/7_all_relation_tags.json" \
+  --all_relation_tags "${ARGUS_INTERIM_ROOT}/7_all_related_tags.json" \
   --main_dataset "${ARGUS_INTERIM_ROOT}/6_main_dataset.jsonl" \
   --out "${ARGUS_INTERIM_ROOT}/8_unrelevant_qids.jsonl"
